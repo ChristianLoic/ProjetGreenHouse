31688f542e8cb2b0d6e80d2b14b8c2613094778a
ChristianLoic <christianloic.atonfack@gmail.com>
2021-09-07 23:10:57 -0400

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot([])],
+  declarations: [AppComponent, HelloComponent, TopBarComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
