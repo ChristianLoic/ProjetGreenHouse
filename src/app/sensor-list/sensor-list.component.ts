fea91c3fed5018087d55b614e80a62e85b976bf7
ChristianLoic <christianloic.atonfack@gmail.com>
2021-09-08 04:17:37 -0400

    2 files modified, 1 added

--- src/app/sensor-list/sensor-list.component.ts
+++ src/app/sensor-list/sensor-list.component.ts
@@ -9,16 +9,22 @@ import { Sensor, sensors } from '../sensors';
 
 
 
+
 @Component({
   selector: 'app-sensor-list',
   templateUrl: './sensor-list.component.html',
   styleUrls: ['./sensor-list.component.css']
 })
 export class SensorListComponent implements OnInit {
-   
+
+   sensors = new Array<Sensor>();
 
   serre : Serre | undefined;
 
+  sensor :Sensor | undefined;
+
+
+   
   constructor(  private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -29,6 +35,21 @@ export class SensorListComponent implements OnInit {
 
       // Find the product that correspond with the id provided in route.
    this.serre = serres.find(serre => serre.idSerre === serreIdFromRoute);
-  }
 
+   
+    // fill the sensors list 
+    
+   for (var i = 0; i<sensors.length; i++)
+   {         
+          this.sensor= sensors.find(sensor=> sensor.idSensor === i)
+          if(this.sensor.idSerre==serreIdFromRoute )
+          {
+              this.sensors.push(this.sensor) 
+          }
+   }
+  
+   
+   
+  }
+  
 }
