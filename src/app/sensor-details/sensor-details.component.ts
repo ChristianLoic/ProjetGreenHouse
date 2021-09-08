8944f0d3ddee73088800be954d6811a1f3c05578
ChristianLoic <christianloic.atonfack@gmail.com>
2021-09-08 13:59:01 -0400

    3 files modified

--- src/app/sensor-details/sensor-details.component.ts
+++ src/app/sensor-details/sensor-details.component.ts
@@ -24,7 +24,7 @@ export class SensorDetailsComponent implements OnInit
 
      //  get the sensor id from the current route.
   const routeParams = this.route.snapshot.paramMap;
-  const sensorIdFromRoute = Number(routeParams.get('productId'));
+  const sensorIdFromRoute = Number(routeParams.get('idSensor'));
 
    // Find the sensor that correspond with the id provided in route.
    this.sensor = sensors.find(sensor => sensor.idSensor === sensorIdFromRoute);
