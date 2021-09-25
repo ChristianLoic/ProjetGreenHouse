074021556f04b21e39469e2ea6d8900fd8a65bb7
ChristianLoic <christianloic.atonfack@gmail.com>
2021-09-25 03:18:48 -0400

    3 files modified

--- src/app/serres.ts
+++ src/app/serres.ts
@@ -1,6 +1,9 @@
 export interface Serre {
   idSerre: number;
   description: string;
+  humiditeSol:number;
+  humiditeAir: number;
+  luminosite:number;
   idProfil: number;
 }
 
@@ -8,241 +11,225 @@ export const serres = [
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 1,
     description: 'banane',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   },
   {
     idSerre: 2,
     description: 'brocoli',
-    idProfil: 1
-  },
-  {
-    idSerre: 1,
-    description: 'banane',
-    idProfil: 1
-  },
-  {
-    idSerre: 2,
-    description: 'brocoli',
-    idProfil: 1
-  },
-  {
-    idSerre: 1,
-    description: 'banane',
-    idProfil: 1
-  },
-  {
-    idSerre: 2,
-    description: 'brocoli',
-    idProfil: 1
-  },
-  {
-    idSerre: 1,
-    description: 'banane',
-    idProfil: 1
-  },
-  {
-    idSerre: 2,
-    description: 'brocoli',
-    idProfil: 1
-  },
-  {
-    idSerre: 1,
-    description: 'banane',
-    idProfil: 1
-  },
-  {
-    idSerre: 2,
-    description: 'brocoli',
-    idProfil: 1
-  },
-  {
-    idSerre: 1,
-    description: 'banane',
-    idProfil: 1
-  },
-  {
-    idSerre: 2,
-    description: 'brocoli',
-    idProfil: 1
-  },
-  {
-    idSerre: 1,
-    description: 'banane',
-    idProfil: 1
-  },
-  {
-    idSerre: 2,
-    description: 'brocoli',
-    idProfil: 1
-  },
-  {
-    idSerre: 1,
-    description: 'banane',
-    idProfil: 1
-  },
-  {
-    idSerre: 2,
-    description: 'brocoli',
-    idProfil: 1
-  },
-  {
-    idSerre: 1,
-    description: 'banane',
-    idProfil: 1
-  },
-  {
-    idSerre: 2,
-    description: 'brocoli',
-    idProfil: 1
-  },
-  {
-    idSerre: 1,
-    description: 'banane',
-    idProfil: 1
-  },
-  {
-    idSerre: 2,
-    description: 'brocoli',
-    idProfil: 1
-  },
-  {
-    idSerre: 1,
-    description: 'banane',
-    idProfil: 1
-  },
-  {
-    idSerre: 2,
-    description: 'brocoli',
+    humiditeSol:30,
+    humiditeAir: 20,
+    luminosite:10,
     idProfil: 1
   }
 ];
