e0508602f1f87203d7be6895e319e747ab589694
ChristianLoic <christianloic.atonfack@gmail.com>
2021-09-25 02:33:41 -0400

    5 files modified

--- src/app/sensors.ts
+++ src/app/sensors.ts
@@ -2,10 +2,7 @@ export interface Sensor
 {
                 idSensor: number;
                 description: string;
-                temperature: number;
-                luminosite: number;
-                humidite: number;
-                typeData: string;
+                valeur : number 
                 idSerre:number;
                 
 }
@@ -15,162 +12,72 @@ export const sensors =
 [
   {
     idSensor: 1,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
+
+    description: 'humidite air',
+    valeur : 30,
     idSerre:1,
   } ,
 
   {
     idSensor: 2,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
+    description: 'luminosite',
+    valeur : 30,
     idSerre:1,
   },
   {
-    
     idSensor: 3,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
-    idSerre:2,
-  } ,
-
-  {
-    idSensor: 4,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
-    idSerre:2,
-  } ,
-  {
-    idSensor: 5,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
-    idSerre:1,
-  } ,
-
-  {
-    idSensor: 6,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
+    description: 'humidite sol',
+    valeur : 30,
     idSerre:1,
   },
-  {
-    idSensor: 7,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
-    idSerre:2,
-  } ,
+  
+ 
+  
 
-  {
-    idSensor: 8,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
-    idSerre:2,
-  } ,
   {
     idSensor: 1,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
-    idSerre:1,
+
+    description: 'humidite air',
+    valeur : 30,
+    idSerre:2,
   } ,
 
   {
     idSensor: 2,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
-    idSerre:1,
+    description: 'luminosite',
+    valeur : 30,
+    idSerre:2,
   },
   {
     idSensor: 3,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
+    description: 'humidite sol',
+    valeur : 30,
     idSerre:2,
-  } ,
-
-  {
-    idSensor: 4,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
-    idSerre:2,
-  } ,
-  {
-    idSensor: 5,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
-    idSerre:1,
-  } ,
-
-  {
-    idSensor: 6,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
-    idSerre:1,
   },
-  {
-    idSensor: 7,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
-    idSerre:2,
-  } ,
-
-  {
-    idSensor: 8,
-    description: 'stri',
-    temperature: 30,
-    luminosite: 20,
-     humidite: 10,
-    typeData: 'string',
-    idSerre:2,
-  } ,
   
 
 
+  {
+    idSensor: 1,
 
+    description: 'humidite air',
+    valeur : 30,
+    idSerre:3,
+  } ,
 
+  {
+    idSensor: 2,
+    description: 'luminosite',
+    valeur : 30,
+    idSerre:3,
+  },
+  {
+    idSensor: 3,
+    description: 'humidite sol',
+    valeur : 30,
+    idSerre:3,
+  },
+  
 
 ]
 
